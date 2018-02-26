9005cc3c9a6602830da74e9790b07d0e952d7f46
amazefan <32010595+amazefan@users.noreply.github.com>
2018-02-26 22:19:54 +0800

    Add files via upload

---
+++ EL/Argparser.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 10:29:32 2018
+
+@author: Fan
+"""
+
+import argparse
+def Argparser():
+    parser = argparse.ArgumentParser(description='PyTorch ELnet Training')
+    parser.add_argument('--arch','-a',default = 'ResNet')
+    parser.add_argument('--cuda','-c',default = True)
+    parser.add_argument('--layer',nargs = 4, type = int,
+                        help = 'Define layes in the Net')
+    parser.add_argument('--epoches',type = int ,default = 1000,
+                        help = 'Define train epoches ')
+    parser.add_argument('--lr' , type = float ,default = 0.01,
+                        help = 'Define origin learning rate')
+    parser.add_argument('--batch_size' , type = int , default = 100,
+                        help = 'Define train batch size')
+    parser.add_argument('--train_folder_NG' , type = str , default = r'E:\hongpucorp\EL\data\defect pic\NG_AUG',
+                        help = 'Load the train data with folder path(NG folder)')
+    parser.add_argument('--train_folder_OK' , type = str , default = r'E:\hongpucorp\EL\data\defect pic\OK',
+                        help = 'Load the train data with folder path(OK folder)')
+    parser.add_argument('--save_folder' , type = str , default = '',
+                        help = 'Folder path to save the model and trainRecord')
+    parser.add_argument('--load_folder' , type = str , default = '',
+                        help = 'Folder path to load the model')
+    parser.add_argument('--print_freq' , type = int , default = 100,
+                        help = 'Frequency to print the train info(loss and accuarcy)')
+    parser.add_argument('--checkpoint' , type = int , default = 100,
+                        help = 'Frequency to save the model')
+    
+    return parser
+
